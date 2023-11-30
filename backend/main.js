637bcea90062ce34b0fdb3bc56507dfd1be4258a
Guillem Casas <gucasassi@gmail.com>
2023-11-30 08:24:49 +0100

    refactor(back): handle unhandled promise rejections

--- backend/main.js
+++ backend/main.js
@@ -28,6 +28,12 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.APP_PORT || 3000;
 
 // Start the server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
