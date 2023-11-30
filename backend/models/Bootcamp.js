549a2d2aa79fcac24608083efe9759eb4bda7a31
Guillem Casas <gucasassi@gmail.com>
2023-11-30 23:50:10 +0100

    feat(back): creat slugify attribute from bootcamp name

--- backend/models/Bootcamp.js
+++ backend/models/Bootcamp.js
@@ -1,3 +1,4 @@
+const slugify = require("slugify");
 const mongoose = require("mongoose");
 
 const BootcampSchema = new mongoose.Schema({
@@ -84,6 +85,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
