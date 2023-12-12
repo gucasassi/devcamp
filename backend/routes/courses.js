c44b4dcdba68508ce260526cdc785563e5170d8a
Guillem Casas <gucasassi@gmail.com>
2023-12-12 08:17:35 +0100

    feat(back): get single course

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,12 +1,17 @@
 // Imports
 const express = require("express");
-const { getCourses } = require("../controllers/courses");
+const { getCourses, getCourse } = require("../controllers/courses");
 
 // Create a router
 const router = express.Router({ mergeParams: true });
 
-// Define routes
+// Root routes
+// GET for fetching all courses
 router.route("/").get(getCourses);
+
+// Individual routes
+// GET for fetching a course by ID
+router.route("/:id").get(getCourse);
 
 // Export router
 module.exports = router;
