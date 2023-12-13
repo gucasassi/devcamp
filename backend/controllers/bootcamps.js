e483a15897ab0e579c94d843a31523c11ae5b63a
Guillem Casas <gucasassi@gmail.com>
2023-12-13 17:07:04 +0100

    feat(back): allow update bootcamp only to owner and admins

--- backend/controllers/bootcamps.js
+++ backend/controllers/bootcamps.js
@@ -56,16 +56,30 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route       PUT /api/v1/bootcamps/:id
 // @access      Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
+  // Check bootcamp exist
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp with id ${req.params.id} does not exist`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User with id ${req.user.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  // Update bootcamp
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
