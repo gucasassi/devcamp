5567f47320ca3da0e909409484e5ff0f0be6d340
Guillem Casas <gucasassi@gmail.com>
2023-12-14 16:23:55 +0100

    chore(reviews): add reviews data and update seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -11,6 +11,7 @@ dotenv.config(".env");
 const User = require("./models/User");
 const Course = require("./models/Course");
 const Bootcamp = require("./models/Bootcamp");
+const Review = require("./models/Review");
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URI);
@@ -29,12 +30,17 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
+
 // Import bootcamps into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log(`Data imported`.green);
     process.exit();
   } catch (err) {
@@ -48,6 +54,7 @@ const deleteData = async () => {
     await Course.deleteMany();
     await User.deleteMany();
     await Bootcamp.deleteMany();
+    await Review.deleteMany();
     console.log(`Data deleted`.yellow);
     process.exit();
   } catch (err) {
