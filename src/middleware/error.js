2ef770fc32d419356bef21e61550c5a6a07d6352
Guillem Casas <gucasassi@users.noreply.github.com>
2025-10-29 08:48:50 +0100

    feat: implement centralized mongoose error handling

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -15,11 +15,25 @@ import ErrorResponse from '../utils/error-response.js';
 const errorHandler = (err, req, res, next) => {
   // Define custom error object.
   let error = { ...err };
-  console.log('error:', error);
+  console.log('err:', err);
+
   // Mongoose ObjectId.
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key.
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error.
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    // We only send the first validation error message for simplicity.
+    error = new ErrorResponse(message[0], 400);
   }
 
   // Return the error response.
